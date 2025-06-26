c751dcf9b27370d26bc0f6a6c4b76d19cd1320c7
gushags <jeff@hagkull.com>
2025-06-26 15:54:08 -0700

    async functions are still beating me

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,21 +1,22 @@
 // pokemon.js
 
 const pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=12';
-let pokemons = [];
 
 async function fetchPokemonList() {
-  const response = await fetch(pokeApiUrl);
-  const pokemonData = await response.json();
-  pokemons = await pokemonData.results.map((element) => {
-    return {
-      name: element.name,
-      imgUrl: `assets/img/${element.name}.png`,
-    };
-  });
-}
-
-function generatePokemons(pokemons) {
-  return pokemons;
+  try {
+    const response = await fetch(pokeApiUrl);
+    const pokemonData = await response.json();
+    const pokemons = await pokemonData.results.map((element) => {
+      return {
+        name: element.name,
+        imgUrl: `assets/img/${element.name}.png`,
+      };
+    });
+    console.log(pokemons); // prints an array of 12 pokemons
+    return pokemons;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 const list = fetchPokemonList();
