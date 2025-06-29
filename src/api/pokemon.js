5f4fd68713cfcd492c63f7f5440a8a614caa042b
gushags <jeff@hagkull.com>
2025-06-28 21:48:38 -0700

    working on async call

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -2,21 +2,32 @@
 
 const pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=12';
 
-async function fetchPokemonList() {
+const fetchPokemonList = async () => {
   try {
     const response = await fetch(pokeApiUrl);
     const pokemonData = await response.json();
     const pokemons = await pokemonData.results.map((element) => {
+      const data = fetchPokemonSprites(element.url);
+      let frontSprite = data.then((e) => e.front_default);
+      let backSprite = data.then((e) => e.back_default);
+
       return {
         name: element.name,
         imgUrl: `assets/img/${element.name}.png`,
+        front: frontSprite,
+        back: backSprite,
       };
     });
-    console.log(pokemons); // prints an array of 12 pokemons
     return pokemons;
   } catch (err) {
     console.error(err);
   }
+};
+
+async function fetchPokemonSprites(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.sprites;
 }
 
 const list = fetchPokemonList();
