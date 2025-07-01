dfe981db98e7bd1afe4d9dec9d8153f2361dbde2
gushags <jeff@hagkull.com>
2025-07-01 13:05:26 -0700

    Complete function to fetch pokemon data

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -2,14 +2,14 @@
 
 const pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=12';
 
-const fetchPokemonList = async () => {
+async function fetchPokemonList() {
   try {
     const response = await fetch(pokeApiUrl);
     const pokemonData = await response.json();
-    const pokemons = await pokemonData.results.map((element) => {
-      const data = fetchPokemonSprites(element.url);
-      let frontSprite = data.then((e) => e.front_default);
-      let backSprite = data.then((e) => e.back_default);
+    const pokemons = pokemonData.results.map(async (element) => {
+      const data = await fetchPokemonSprites(element.url);
+      let frontSprite = data.front_default;
+      let backSprite = data.back_default;
 
       return {
         name: element.name,
@@ -22,7 +22,7 @@ const fetchPokemonList = async () => {
   } catch (err) {
     console.error(err);
   }
-};
+}
 
 async function fetchPokemonSprites(url) {
   const response = await fetch(url);
