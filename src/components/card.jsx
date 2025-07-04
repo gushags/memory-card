59865abb4c1a36a3dfe3a5b28e646678d8b8d056
gushags <jeff@hagkull.com>
2025-07-04 08:34:12 -0700

    Update card function

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,8 +1,16 @@
 function Card({ card }) {
   return (
-    <div className='card'>
-      <img src={card.imgUrl} alt={card.alt} />
+    <div className='card' key={card.key}>
+      <div className='top-sprites'>
+        <img src={card.front} />
+        <img src={card.back} alt='' />
+      </div>
+      <img src={card.imgUrl} alt={card.name} />
       <h3 className='title'>{card.name}</h3>
+      <div className='bottom-sprites'>
+        <img src={card.front} />
+        <img src={card.back} alt='' />
+      </div>
     </div>
   );
 }
